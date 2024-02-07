99c61ef745e088ba559ee24fd8fd091ff29fd5df
tobias maguna <tobimaguna@gmail.com>
2024-02-07 14:10:19 -0300

    resolvi ejercicios de entrada y salida 01

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -26,8 +26,10 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
-gggg
+        alert("titulo", "Esto no anda, funciona")
+    
+
+    
 
 if __name__ == "__main__":
     app = App()
