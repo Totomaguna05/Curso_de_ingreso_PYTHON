5d782d8f529cf794421e39ff3ca16e1b731b0c90
tobias maguna <tobimaguna@gmail.com>
2024-02-08 22:07:04 -0300

    ejercicio 10

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_10.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_10.py
@@ -34,8 +34,17 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, padx=30, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        importe = self.txt_importe.get()
+        importe = float(importe)
+        
+        calculo = importe * 0.20
+        importe_actualizado = importe - calculo
+        mensaje = f"el importe acualizado con el descuento es de $" + str (importe_actualizado)
+        alert("Sueldo", mensaje)
 
+        self.txt_importe.delete(0,"end")
+        
+        
 
 if __name__ == "__main__":
     app = App()
